4d21fbf0a45d269ee08909408a540eb27a60e3d7
Facundo Fierro <sheins.sb@gmail.com>
2023-11-18 11:35:10 -0300

    mongo db uri updated

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const  connectDB = async () =>{
     try {
-       await  mongoose.connect('mongodb://localhost/baratie')
+       await  mongoose.connect(process.env.MONGODB_URI)
        console.log('db connected')
     } catch (error) {
         console.log(error)
